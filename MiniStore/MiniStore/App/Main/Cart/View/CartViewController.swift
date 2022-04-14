599c29c16c803728af59b07376e559141a94f9a6
Ewan <ewanthomas00@gmail.com>
2022-04-14 14:18:49 +0200

    Display tab bar view controller, products view controller, cart view controller (#2)
    * Adds project group structure + stub view controllers
    
    * removes redundant storyboard and view controller
    
    * Displays view controllers

---
+++ MiniStore/MiniStore/App/Main/Cart/View/CartViewController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+final class CartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ MiniStore/MiniStore/App/Main/Product/View/ProductViewController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+final class ProductViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
