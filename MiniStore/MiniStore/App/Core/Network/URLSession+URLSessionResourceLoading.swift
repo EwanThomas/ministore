aa85d9325fbbc72c0d4c22a43317b12224526072
Ewan Thomas <ewanthomas00@gmail.com>
2022-04-16 14:08:41 +0200

    removes completionHandler shadow

--- MiniStore/MiniStore/App/Core/Network/URLSession+URLSessionResourceLoading.swift
+++ MiniStore/MiniStore/App/Core/Network/URLSession+URLSessionResourceLoading.swift
@@ -8,11 +8,4 @@ extension URLSession: URLSessionResourceLoading {
     func loadResource(url: URL) async throws -> (Data ,URLResponse)  {
         return try await data(from: url)
     }
-    
-    func loadResource(url: URL, completionHandler: @escaping (Data? ,Error?) -> Void) {
-        let dataTask = dataTask(with: url) { data, _, error in
-            completionHandler(data, error)
-        }
-        dataTask.resume()
-    }
 }
